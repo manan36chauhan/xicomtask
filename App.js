f6b76d60b2bab1f80006842fefe04d1efd40e236
Manan Chauhan <63107421+manan36chauhan@users.noreply.github.com>
2024-03-22 13:16:36 +0530

    Update App.js

--- App.js
+++ App.js
@@ -11,48 +11,78 @@ import {
 
 const App = () => {
   const [images, setImages] = useState([]);
+  const [offset, setOffset] = useState(1);
 
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    console.log('function called');
-    const url = 'https://picsum.photos/v2/list?page=1&limit=5'; // Removed space in URL
+    // const url = 'https://picsum.photos/v2/list?page=1&limit=5'; // Removed space in URL
+    const url = 'https://dev3.xicom.us/xttest/getdata.php';
     try {
-      let response = await fetch(url);
-      let data = await response.json();
-      console.log(data);
-      setImages(data);
+      let formdata = new FormData();
+      formdata.append('user_id', '108');
+      formdata.append('offset', offset);
+      formdata.append('type', 'popular');
+
+      const requestOptions = {
+        method: 'POST',
+        headers: {'Content-Type': 'multipart/form-data'},
+        body: formdata,
+      };
+      fetch(url, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          // console.log('data.images------------', data.images);
+          let img = [];
+          if (images.length == 0) {
+            img = data.images;
+          } else {
+            img = images;
+            data.images.forEach(element => {
+            img.push(element);
+              
+            });
+          }
+          console.log('img----------------', img);
+          setImages(img);
+        });
     } catch (error) {
       console.error('Error:', error);
-      alert('Error: ' + error.message);
     }
   };
 
   const loadMoreImages = () => {
-    console.log(images.length);
-    console.log(JSON.stringify(images)); // Convert images array to JSON string
+    console.log('offset------------', offset);
+    setOffset(offset + 1);
+    fetchData();
+    // console.log(images.length);
+    // console.log(JSON.stringify(images)); // Convert images array to JSON string
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Random Images</Text>
+
       <ScrollView>
         {images.map((image, index) => (
           <TouchableOpacity
-            onPress={() => navigation.navigate('ImageDetailsScreen', {image})}>
+          // onPress={() => navigation.navigate('ImageDetailsScreen', { image })}
+          >
             <Image
-              key={index}
-              source={{uri: image.download_url}}
-              style={{width: '100%', aspectRatio: image.width / image.height}}
+              source={{
+                uri: image.xt_image,
+              }}
+              style={{width: 350, height: 350}}
             />
           </TouchableOpacity>
+          // <Text>{image.id}</Text>
         ))}
       </ScrollView>
 
       {/* Corrected the onPress event */}
-      <Button title="Load more" onPress={loadMoreImages()} />
+      <Button title="Load more" onPress={() => loadMoreImages()} />
     </View>
   );
 };
@@ -70,5 +100,9 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 10,
+  },
+  img_list: {
+    width: '200px',
+    height: '200px',
   },
 });
